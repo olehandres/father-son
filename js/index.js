adcc2c759320d599f555fdc08f187cabc7ef1cc2
Oleh Andres <olehandres@icloud.com>
2018-11-05 13:14:46 +0000

    Fix JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Get a seat
 document.querySelector('.form input[type=submit]').addEventListener('click', submitForm);
 
 function submitForm (e) {
@@ -16,3 +17,35 @@ function submitForm (e) {
         xhr.send(data);
     
 }
+
+      
+  //Youtube    
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '360',
+          width: '640',
+          videoId: 'Y-BNGdD_mDQ',
+          events: {
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 97000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
--- reservation.php
+++ reservation.php
@@ -1,8 +1,7 @@
 Welcome <?php echo $_POST["write-a-name"]; ?><br>
 Your email address is: <?php echo $_POST["write-email"]; ?><br>
 Your phone number is: <?php echo $_POST["write-phone"]; ?><br>
-Your barber is <?php echo $_POST["select-barber"]; ?><br>
-Your service is <?php echo $_POST["select-service"]; ?><br>
-Date <?php echo $_POST["calendar"]; ?><br>
-Time <?php echo $_POST["time1"]; ?><br>
+Your barber is: <?php echo $_POST["select-barber"]; ?><br>
+Date: <?php echo $_POST["calendar"]; ?><br>
+Time: <?php echo $_POST["time1"]; ?><br>
 
